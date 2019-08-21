aec13bd69f32cb04d8beecfe35904fe906b002d5
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2019-08-21 15:10:37 +0200

    make rubocop happy

--- spec/classes/mollyguard_spec.rb
+++ spec/classes/mollyguard_spec.rb
@@ -3,29 +3,30 @@ require 'spec_helper'
 
 describe 'mollyguard' do
   let :default_params do
-     { :package_ensure    => 'present',
-       :packages          => ['molly-guard'],
-       :checks            => {},
-       :check_destination => '/etc/molly-guard/run.d',
-       :purge_checks      => true,
-       :ignore_sys_checks => ['10-print-message', '30-query-hostname'],
-     }
+    { package_ensure: 'present',
+      packages: ['molly-guard'],
+      checks: {},
+      check_destination: '/etc/molly-guard/run.d',
+      purge_checks: true,
+      ignore_sys_checks: ['10-print-message', '30-query-hostname'] }
   end
 
   shared_examples 'mollyguard shared example' do
     it { is_expected.to compile.with_all_deps }
 
-    it { is_expected.to contain_package( 'molly-guard')
-      .with_ensure(params[:package_ensure])
-      .with_tag('molly-guard-packages')
+    it {
+      is_expected.to contain_package('molly-guard')
+        .with_ensure(params[:package_ensure])
+        .with_tag('molly-guard-packages')
     }
   end
 
   shared_examples 'mollyguard purge checks shared example' do
-    it { is_expected.to contain_file( params[:check_destination] )
-      .with_ensure('directory')
-      .with_purge(true)
-      .with_recurse(true)
+    it {
+      is_expected.to contain_file(params[:check_destination])
+        .with_ensure('directory')
+        .with_purge(true)
+        .with_recurse(true)
     }
   end
 
@@ -33,49 +34,49 @@ describe 'mollyguard' do
     let :params do
       default_params
     end
+
     it_behaves_like 'mollyguard shared example'
     it_behaves_like 'mollyguard purge checks shared example'
 
-    it { is_expected.to contain_file( params[:check_destination] + '/10-print-message' ) }
-    it { is_expected.to contain_file( params[:check_destination] + '/30-query-hostname' ) }
+    it { is_expected.to contain_file(params[:check_destination] + '/10-print-message') }
+    it { is_expected.to contain_file(params[:check_destination] + '/30-query-hostname') }
   end
 
   context 'with non defaults' do
     let :params do
       default_params.merge(
-	:package_ensure    => 'installed',
-        :packages          => ['molly-guard', 'blah'],
-        :check_destination => '/tmp',
-        :ignore_sys_checks => ['30-whatever', '30-another'],
+        package_ensure: 'installed',
+        packages: ['molly-guard', 'blah'],
+        check_destination: '/tmp',
+        ignore_sys_checks: ['30-whatever', '30-another'],
       )
     end
+
     it_behaves_like 'mollyguard shared example'
     it_behaves_like 'mollyguard purge checks shared example'
 
-    it { is_expected.to contain_file( params[:check_destination] + '/30-whatever' ) }
-    it { is_expected.to contain_file( params[:check_destination] + '/30-another' ) }
+    it { is_expected.to contain_file(params[:check_destination] + '/30-whatever') }
+    it { is_expected.to contain_file(params[:check_destination] + '/30-another') }
 
-    it { is_expected.to contain_package( 'blah')
-      .with_ensure(params[:package_ensure])
-      .with_tag('molly-guard-packages')
+    it {
+      is_expected.to contain_package('blah')
+        .with_ensure(params[:package_ensure])
+        .with_tag('molly-guard-packages')
     }
   end
 
   context 'with non purging checks' do
     let :params do
       default_params.merge(
-	:purge_checks    => false,
-        :check_destination => '/tmp',
+        purge_checks: false,
+        check_destination: '/tmp',
       )
     end
+
     it_behaves_like 'mollyguard shared example'
 
-    it { is_expected.to_not contain_file( params[:check_destination] )
-    }
-
-    it { is_expected.to_not contain_file( params[:check_destination]) }
-    it { is_expected.to_not contain_file( params[:check_destination] + '/10-print-message' ) }
-    it { is_expected.to_not contain_file( params[:check_destination] + '/30-query-hostname' ) }
+    it { is_expected.not_to contain_file(params[:check_destination]) }
+    it { is_expected.not_to contain_file(params[:check_destination] + '/10-print-message') }
+    it { is_expected.not_to contain_file(params[:check_destination] + '/30-query-hostname') }
   end
 end
-
--- spec/defines/checks_drbd_spec.rb
+++ spec/defines/checks_drbd_spec.rb
@@ -2,54 +2,51 @@
 require 'spec_helper'
 
 describe 'mollyguard::checks::drbd' do
-
   let :default_params do
-     { :destination => '/tmp/test',
-       :check_name  => 'ceph-molly-test',
-       :sort        => '20',
-       :owner       => 'root',
-       :group       => 'root',
-       :mode        => '0755',
-     }
+    { destination: '/tmp/test',
+      check_name: 'ceph-molly-test',
+      sort: '20',
+      owner: 'root',
+      group: 'root',
+      mode: '0755' }
   end
 
-
   shared_examples 'mollyguard::checks::drbd define' do
-
     context 'it compiles with all dependencies' do
       it { is_expected.to compile.with_all_deps }
     end
- 
-    it { is_expected.to contain_file( params[:destination] + '/' + params[:sort] + '-' + params[:check_name] )
-      .with_ensure('present')
-      .with_owner(params[:owner])
-      .with_group(params[:group])
-      .with_mode(params[:mode])
+
+    it {
+      is_expected.to contain_file(params[:destination] + '/' + params[:sort] + '-' + params[:check_name])
+        .with_ensure('present')
+        .with_owner(params[:owner])
+        .with_group(params[:group])
+        .with_mode(params[:mode])
     }
   end
 
   context 'whith defaults' do
-    let (:title) { 'test-check' }
+    let(:title) { 'test-check' }
     let :params do
       default_params
     end
-    it_behaves_like 'mollyguard::checks::drbd define'
 
+    it_behaves_like 'mollyguard::checks::drbd define'
   end
 
   context 'whith non-defaults' do
-    let (:title) { 'another-check' }
+    let(:title) { 'another-check' }
     let :params do
-      default_params.merge( 
-       :destination => '/tmp/somewhereelse',
-       :check_name => 'another-blah-check',
-       :sort        => '42',
-       :owner       => 'someone',
-       :group       => 'someone',
-       :mode        => '4242',
+      default_params.merge(
+        destination: '/tmp/somewhereelse',
+        check_name: 'another-blah-check',
+        sort: '42',
+        owner: 'someone',
+        group: 'someone',
+        mode: '4242',
       )
     end
+
     it_behaves_like 'mollyguard::checks::drbd define'
   end
 end
-
