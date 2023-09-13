8266e7015d9d5307da942ed60ed08707ad21478d
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2023-09-13 11:50:01 +0200

    add rabbitmq check

---
+++ manifests/checks/rabbitmq.pp
@@ -0,0 +1,32 @@
+#
+# use this to add drbd check to molly-guard
+#
+# @param destination
+#   directory where to put the check (mandatory)
+# @param check_name
+#   name of the check (defaults to $title)
+# @param sort
+#   sort parameter (defaults to '20')
+# @param owner
+#   owner of the check (file), defaults to 'root'
+# @param group
+#   group of the check (file), defaults to 'root'
+# @param mode
+#   mode of the check (file), defaults to '0755'
+#
+define mollyguard::checks::rabbitmq (
+  String  $destination,
+  String  $check_name = $title,
+  String  $sort       = '20',
+  String  $owner      = 'root',
+  String  $group      = 'root',
+  String  $mode       = '0755',
+) {
+  file { "${destination}/${sort}-${check_name}":
+    ensure  => 'file',
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    content => epp('mollyguard/checks/check-rabbitmq.sh.epp', {}),
+  }
+}
